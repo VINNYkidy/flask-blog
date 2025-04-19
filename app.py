455a0c98575f17007be118d48bef3407574078de
VINNYkidy <VINNYkidy>
2025-04-19 06:04:36 +0300

    added dynamic blog posts

--- app.py
+++ app.py
@@ -1,58 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.secret_key = 'secretkey'  # Required for login sessions
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
 
+# Sample blog posts (later we can store them in a database)
+posts = [
+    {
+        'title': 'My First Blog Post',
+        'content': 'This is my first post on Flask!',
+        'author': 'Vinny'
+    },
+    {
+        'title': 'Learning Flask',
+        'content': 'Today I learned how to use templates in Flask.',
+        'author': 'Vinny'
+    }
+]
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+@app.route('/')
+def home():
+    return render_template('home.html', posts=posts)
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = generate_password_hash(request.form['password'])
-        user = User(username=username, password=password)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('login'))
-    return render_template('register.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        user = User.query.filter_by(username=request.form['username']).first()
-        if user and check_password_hash(user.password, request.form['password']):
-            login_user(user)
-            return redirect(url_for('index'))
-    return render_template('login.html')
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
-
-@app.route('/create', methods=['GET', 'POST'])
-@login_required
-def create():
-    ...
-
-@app.route('/edit/<int:post_id>', methods=['GET', 'POST'])
-@login_required
-def edit(post_id):
-    ...
-
-@app.route('/delete/<int:post_id>', methods=['POST'])
-@login_required
-def delete(post_id):
-    ...
-
+@app.route('/about')
+def about():
+    return render_template('about.html')
