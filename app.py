017abc9e61458dcb92efd9b5e1fa5fc4e00b38ac
VINNYkidy <mapundadavid34@gmail.com>
2025-05-18 07:38:45 +0300

    fix app definition and blog post creation

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-app = flask(VINNYkidy)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
 
 @app.route('/create', methods=['GET', 'POST'])
 def create():
@@ -12,3 +20,11 @@ def create():
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('create.html')
+
+@app.route('/')
+def index():
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
+
+if __name__ == '__main__':
+    app.run(debug=True)
